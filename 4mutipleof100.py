8fe936d2370a436dfa2cf82d26c6dedda291cf62
Angelique Abacajan <ma_utsey@yahoo.com>
2024-09-03 13:58:22 -0400

    Completed CIS-115-140 Homeworks 1-4

---
+++ 4mutipleof100.py
@@ -0,0 +1,13 @@
+'''
+Take a positive integer as an input.  If the number is less than 100, print “less than 100” and then print whether or not it is a multiple of 10.  If the number is greater than 100, print “greater than 100” and print whether or not it is a multiple of 100.
+'''
+
+if __name__ == "__main__":
+    num = int(input("Enter a positive integer: "))
+    
+    if num < 100:
+        print("less than 100")
+        print(num % 10 == 0)
+    else:
+        print("greater than 100")
+        print(num % 100 == 0)
